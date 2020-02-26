f971b2fbec7dc0b13d4a388023bfe394f9cd9410
azamat <@stunitas.com>
2020-02-26 14:11:46 +0900

    densenet is done

--- network.py
+++ network.py
@@ -9,10 +9,10 @@ class DenseBlock(nn.Module):
         super(DenseBlock,self).__init__()
         self.bn1=nn.BatchNorm2d(inchannel)
         self.act1=nn.ReLU(inplace=True)
-        self.conv1=nn.Conv2d(inchannel,outchannel,kernel_size=1,stride=1,padding=0,bias=False)
-        self.bn2=nn.BatchNorm2d(outchannel)
+        self.conv1=nn.Conv2d(inchannel,4*outchannel,kernel_size=1,stride=1,padding=0,bias=False)
+        self.bn2=nn.BatchNorm2d(4*outchannel)
         self.act2=nn.ReLU(inplace=True)
-        self.conv2=nn.Conv2d(outchannel,outchannel,kernel_size=3,stride=1,padding=1,bias=False)
+        self.conv2=nn.Conv2d(4*outchannel,outchannel,kernel_size=3,stride=1,padding=1,bias=False)
         
 
     def forward(self,x):
@@ -42,8 +42,9 @@ class DenseLayer(nn.Module):
         return self.layer(x)
 
 class TransitionLayer(nn.Module):
-    def __init__(self,inchannel,outchannel):
+    def __init__(self,inchannel):
         super(TransitionLayer,self).__init__()
+        outchannel=inchannel//2
         self.bn1=nn.BatchNorm2d(inchannel)
         self.act1=nn.ReLU(inplace=True)
         self.conv1=nn.Conv2d(inchannel,outchannel,kernel_size=1,stride=1,padding=0,bias=False)
@@ -57,25 +58,37 @@ class TransitionLayer(nn.Module):
         return out
 
 class DenseNet(nn.Module):
-    def __init__(self,layers):
+    def __init__(self,layers,inchannel,outchannel,growth,num_classes=1000):
+        super(DenseNet,self).__init__()
         self.conv0=nn.Conv2d(inchannel,outchannel,kernel_size=7,stride=2,padding=3,bias=False)
         self.bn0=nn.BatchNorm2d(outchannel)
         self.act0=nn.ReLU(inplace=True)
         self.pool0=nn.MaxPool2d(kernel_size=3,stride=2,padding=1)
-        self.layer1=self._make_dense_layer(layers[0],inchannel,growth)
-        self.tr_layer1=TransitionalLayer(inchannel+layers[0]*growth,inchannel)
-        self.layer2=self._make_dense_layer(layers[1],inchannel,growth)
-        self.tr_layer2=TransitionalLayer(inchannel+layers[1]*growth,inchannel)
-        self.layer3=self._make_dense_layer(layers[2],inchannel,growth)
-        self.tr_layer3=TransitionalLayer(inchannel+layers[2]*growth,inchannel)
-        self.layer4=self._make_dense_layer(layers[3],inchannel,growth)
+        self.layer1=DenseLayer(outchannel,growth,layers[0])
+        self.tr_layer1=TransitionLayer(outchannel+layers[0]*growth)
+        outchannel=(outchannel+layers[0]*growth)//2
+        self.layer2=DenseLayer(outchannel,growth,layers[1])
+        self.tr_layer2=TransitionLayer(outchannel+layers[1]*growth)
+        outchannel=(outchannel+layers[1]*growth)//2
+        self.layer3=DenseLayer(outchannel,growth,layers[2])
+        self.tr_layer3=TransitionLayer(outchannel+layers[2]*growth)
+        outchannel=(outchannel+layers[2]*growth)//2
+        self.layer4=DenseLayer(outchannel,growth,layers[3])
+        self.bn1=nn.BatchNorm2d(outchannel+growth*layers[3])
         self.globpooling=nn.AdaptiveAvgPool2d((1,1))
-        #self.layer5=nn.Linear(,num_classes)
+        self.layer5=nn.Linear(outchannel+growth*layers[3],num_classes)
 
-    def _make_dense_layer(self,num_blocks,inchannel,outchannel):
-
-
-    def forward(self,x)
+    def forward(self,x):
+        out=self.conv0(x)
+        out=self.act0(self.bn0(out))
+        out=self.pool0(out)
+        out=self.tr_layer1(self.layer1(out))
+        out=self.tr_layer2(self.layer2(out))
+        out=self.tr_layer3(self.layer3(out))
+        out=self.globpooling(self.bn1(self.layer4(out)))
+        out=torch.flatten(out,1)
+        out=self.layer5(out)
+        return out
 
 
 if __name__=="__main__":
@@ -85,5 +98,8 @@ if __name__=="__main__":
     denseLayer=DenseLayer(64,32,6)
     summary(denseLayer,(64,56,56))
 
-    tranLayer=TransitionLayer(256,64)
+    tranLayer=TransitionLayer(256)
     summary(tranLayer,(256,56,56))
+
+    densenet=DenseNet([6,12,24,16],3,64,32)
+    summary(densenet,(3,224,224))
